bac9a9433cf71f5681d0afd5921e934b68675d56
saburday <yteslenko@yahoo.com>
2017-10-13 16:11:10 +0300

    SPA5_components

--- notes-app/client/main.js
+++ notes-app/client/main.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App.jsx';
+
 ReactDOM.render(
-  <h1>Notes</h1>,
+  <App />,
   document.getElementById('mount-point')
 );
